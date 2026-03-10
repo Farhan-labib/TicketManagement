8e134b317fb9a7907904e6e21d90384dceb6afae
Farhan Labib <jahin2000.11.15@gmail.com>
2026-03-10 13:30:45 +0600

    swagger

--- TicketManagement/Program.cs
+++ TicketManagement/Program.cs
@@ -6,9 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var provider = builder.Services.BuildServiceProvider();
-var config = provider.GetRequiredService<IConfiguration>();
-builder.Services.AddDbContext<ticket_managementContext>(item => item.UseSqlServer(config.GetConnectionString("dbcs")));
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Ticket Management API",
+        Version = "v1",
+        Description = "API for managing support tickets"
+    });
+});
+
+builder.Services.AddDbContext<ticket_managementContext>(item => item.UseSqlServer(builder.Configuration.GetConnectionString("Dbcs")));
 
 var app = builder.Build();
 
@@ -19,6 +29,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -27,8 +41,18 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Configure Swagger middleware
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ticket Management API V1");
+    c.RoutePrefix = "swagger";
+});
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllers();
 
 app.Run();
